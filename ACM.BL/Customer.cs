8e4c5814d5e7eecb90ea39b336fc8d53b2112832
mattoes <matthewstephens505@gmail.com>
2016-09-30 17:08:09 +0100

    First commit

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+
+        public Customer()
+        {
+
+        }
+
+
+        public Customer(int customerId)
+        {
+            this.CustomerId = customerId;
+        }
+
+        private string _lastName; //Backing field
+
+        public string LastName //Property
+        {
+            get // Used to return the backing field _lastName, you can include code to perform any opperations prior to returning the value
+                //i.e code to reformat the data or verify the user is able to access this data (permissions)
+            {
+                // Any code here
+                return _lastName;
+            }
+            set // This is accessed when the code assigns a value to the property, can add code to validate the value before assigning it
+            {
+                // Any code here
+                _lastName = value;
+            }
+        }
+
+        public static int InstanceCount { get; set; }
+
+        public string FirstName { get; set; }
+        public string EmailAddress { get; set; }
+        public int CustomerId { get; private set; } //private set: We can set the property in the code but any code external to this class cannot
+
+        public string FullName
+        {
+            get
+            {
+                string fullname = LastName;
+
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullname))
+                    {
+                        fullname += ", ";
+                    }
+                    fullname += FirstName;
+                }
+                return fullname;
+            }
+
+        }
+
+
+        public Customer Retrieve(int customerId)
+        {
+            return new Customer();
+        }
+
+        public List<Customer> Retrieve()
+        {
+            return new List<Customer>();
+        }
+
+        public bool Save()
+        {
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(LastName)) isValid = false;
+            if (string.IsNullOrWhiteSpace(EmailAddress)) isValid = false;
+
+            return isValid;
+        }
+
+
+    }
+}
---
+++ ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ACM.BL;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+
+            //--Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Matt";
+            customer.LastName = "Stephens";
+
+            string expected = "Stephens, Matt";
+
+            //-- Act
+            string actual = customer.FullName;
+
+            //--Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            //Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Matt";
+            string expected = "Matt";
+
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            //Arrange
+            Customer customer = new Customer();
+            customer.LastName = "Stephens";
+            string expected = "Stephens";
+
+            //Act
+            string actual = customer.FullName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+
+            //--Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Matthew";
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer();
+            c2.FirstName = "Hannah";
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer();
+            c3.FirstName = "Millie";
+            Customer.InstanceCount += 1;
+
+            //--Act
+
+
+            //--Assert
+            Assert.AreEqual(Customer.InstanceCount, 3);
+
+        }
+
+        [TestMethod]
+        public void ValidateValid()
+        {
+
+            //-- Arrange
+            var customer = new Customer();
+            customer.LastName = "Stephens";
+            customer.EmailAddress = "matthew.stephens@reedonline.co.uk";
+
+            var expected = true;
+
+            var actual = customer.Validate();
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void ValidateMissingLastName()
+        {
+            //--Arrange
+
+           var customer = new Customer();
+           customer.LastName = "";
+
+            var expected = false;
+
+            //--Act
+            var actual = customer.Validate();
+
+            //--Assert
+            Assert.AreEqual(actual, expected);
+
+
+        }
+
+        [TestMethod]
+        public void ValidateMissingEmailAddress()
+        {
+            //-Arrange
+            var customer = new Customer();
+            customer.EmailAddress = "";
+
+            var expected = false;
+
+            //--Act
+            var actual = customer.Validate();
+
+            //--Assert
+            Assert.AreEqual(actual, expected);
+
+
+        }
+    }
+}
